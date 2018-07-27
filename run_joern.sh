24ed1ae021269c1a570c0161f5cbb3452498eefa
Jones <rjones17@SDGL1608a5164.local>
2018-07-27 10:55:29 -0700

    Modify docker command to use more heap memory

--- run_joern.sh
+++ run_joern.sh
@@ -12,7 +12,7 @@ if [ $# -gt 0 ]; then
     if [ ! -d "$1/.joernIndex" ]; then
 	echo -e "${BLUE}Looks like we need to import the code first.${NC}"
 	echo -e "Enjoy some beer while you wait \xF0\x9F\x8D\xBA \xF0\x9F\x8D\xBA \xF0\x9F\x8D\xBA\n\n"
-	docker run -v $1:/code -p 7474:7474 --rm -w /code  -it neepl/joern java -jar /joern/bin/joern.jar .
+	docker run -v $1:/code -p 7474:7474 --rm -w /code  -it neepl/joern java -d64 -Xmx4g -jar /joern/bin/joern.jar .
     fi
     
     echo -e "\n${BLUE}Firing up neo4j!  You can access it at 0.0.0.0:7474${NC}\n"
